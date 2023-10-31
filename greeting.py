210635d7012283232012c7a00970fdb6aaba1b28
cd-hgupta <114923218+cd-hgupta@users.noreply.github.com>
2023-10-31 12:47:23 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,10 @@
+import json
+
+def lambda_handler(event, context):
+    name = event.get('name', 'World')
+    greeting = f"Hello, {name}!"
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(greeting)
+    }
